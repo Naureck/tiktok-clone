6f3de5634745d029f63a0196b65b136daae9b1cc
StonyStalker <nauihuynh123@gmail.com>
2022-09-19 09:09:36 +0700

    Setup for layout website

---
+++ src/Component/layouts/HeaderOnly/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header"
+
+function DefalutLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DefalutLayout
